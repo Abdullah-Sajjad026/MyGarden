e931aaed9dee0439bb257cc63dba208a02525ec5
Abdullah Sajjad <abdullahsa026@gmail.com>
2023-04-26 08:21:12 +0500

    fix(mobile nav): fixed non responsive conjested mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,15 @@ import clsx from 'clsx'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Link from './Link'
-import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import Image from './Image'
 import { useRouter } from 'next/router'
 
-const Header = () => {
+const Header = ({ onToggleNav }) => {
   const router = useRouter()
 
   return (
-    <header className="supports-backdrop-blur:bg-white/95 sticky top-0 z-40 overflow-x-hidden bg-white/75 py-3 backdrop-blur dark:bg-dark/75">
+    <header className="supports-backdrop-blur:bg-white/95 sticky top-0 z-30 bg-white/75 py-3 backdrop-blur dark:bg-dark/75">
       <div className="mx-auto flex max-w-3xl items-center justify-between px-3 xl:max-w-5xl xl:px-0">
         <div>
           <Link href="/" aria-label={siteMetadata.headerTitle}>
@@ -56,7 +55,24 @@ const Header = () => {
             })}
           </div>
           <ThemeSwitch />
-          <MobileNav />
+          <button
+            className="sm:hidden ml-1 mr-1 h-8 w-8 rounded"
+            aria-label="Toggle Menu"
+            onClick={onToggleNav}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="text-gray-900 dark:text-gray-100"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </header>
--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,9 +1,10 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { Outfit } from '@next/font/google'
 import { MAIN_CONTENT_MIN_HEIGHT } from '@/constants'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import Header from './Header'
+import MobileNav from './MobileNav'
 
 interface Props {
   children: ReactNode
@@ -14,9 +15,14 @@ const outfit = Outfit({
 })
 
 const LayoutWrapper = ({ children }: Props) => {
+  const [showMobileNav, setShowMobileNav] = useState(false)
+
+  const onToggleMobileNav = () => setShowMobileNav((prev) => !prev)
+
   return (
     <div className={`${outfit.className} font-sans`}>
-      <Header />
+      <MobileNav showNav={showMobileNav} onToggleNav={onToggleMobileNav} />
+      <Header onToggleNav={onToggleMobileNav} />
       <SectionContainer>
         <div className="flex flex-col justify-between">
           <main style={{ minHeight: MAIN_CONTENT_MIN_HEIGHT }}>{children}</main>
