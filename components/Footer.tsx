b8896b28e33248d14fae8a4e6ed2a60b3960da99
Abdullah Sajjad <abdullahsa026@gmail.com>
2023-04-29 19:35:10 +0500

    feat(social banner, about me ): added new social banner and updated about me

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,24 @@
-import Link from './Link'
+// import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import { formatDate } from 'pliny/utils/formatDate'
 
 export default function Footer() {
+  const today = new Date()
+
+  const dayName = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesay',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  }[today.getDay()]
+
   return (
     <footer>
-      <div className="mt-16 flex flex-col items-center">
+      <div className="mb-6 mt-12 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
@@ -17,9 +30,9 @@ export default function Footer() {
         <div className="mb-2 flex flex-col items-center space-x-2 text-sm text-gray-500 dark:text-gray-400 sm:flex-row">
           <div>{siteMetadata.author}</div>
           <div className="hidden sm:inline-block">{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{`© ${today.getFullYear()}`}</div>
           <div className="hidden sm:inline-block">{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <div>Have a good {dayName}</div>
         </div>
         {/* <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
